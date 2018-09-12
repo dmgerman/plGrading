25c7592641f111e1c1567027509681dcd5d383fe
D German <dmg@uvic.ca>
2018-09-12 11:40:00 -0700

    added usage and example

---
+++ ccfinderSibling/test/test_1.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+int main(void) {
+    for(int i=0;i++;i<10) {
+        printf("Hello world");
+    }
+    return 10+20;
+}
---
+++ ccfinderSibling/test/test_2.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main(void) {
+    for(int i=0;i++;i<10) {
+        fprintf(stdout, "Hello world");
+
+    }
+    return 10+20;
+}
